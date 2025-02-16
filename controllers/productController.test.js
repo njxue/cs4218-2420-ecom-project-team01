0e15c4f948863a33083059cd8b1d940735f566de
njxue <ngjxue@gmail.com>
2025-02-16 13:26:37 +0800

    Add unit tests for updateProductController

--- controllers/productController.test.js
+++ controllers/productController.test.js
@@ -1,5 +1,8 @@
 import { expect, jest } from "@jest/globals";
-import { createProductController } from "./productController";
+import {
+  createProductController,
+  updateProductController,
+} from "./productController";
 import productModel from "../models/productModel";
 import fs from "fs";
 import slugify from "slugify";
@@ -124,6 +127,133 @@ describe("Create Product Controller Test", () => {
     req.files.photo.size = 1000001;
 
     await createProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({
+      error: "photo is Required and should be less then 1mb",
+    });
+  });
+});
+
+describe("Create Product Controller Test", () => {
+  let req, res, mockProduct;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    slugify.mockImplementation((str) => str);
+
+    mockProduct = {
+      name: "Updated Cool product",
+      description: "This is an updated cool product",
+      price: 100,
+      category: "updated category",
+      quantity: 10,
+      shipping: false,
+      photo: {
+        data: "fakeData",
+        contentType: "image/jpeg",
+      },
+    };
+
+    const { name, description, price, category, quantity, photo } = mockProduct;
+
+    req = {
+      fields: {
+        name,
+        description,
+        price,
+        category,
+        quantity,
+      },
+      files: {
+        photo: {
+          size: 1000000, // Maximum valid size
+          path: "/fakepath/images/photo",
+          type: photo.type,
+        },
+      },
+      params: { id: 1 },
+    };
+
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+  });
+
+  test("successfully updates and saves product when all fields are valid", async () => {
+    fs.readFileSync = jest.fn();
+    const mockProductInstance = { ...mockProduct, save: jest.fn() };
+    productModel.findByIdAndUpdate = jest
+      .fn()
+      .mockResolvedValue(mockProductInstance);
+
+    await updateProductController(req, res);
+
+    expect(productModel.findByIdAndUpdate).toHaveBeenCalledWith(
+      req.params.pid,
+      { ...req.fields, slug: slugify(req.fields.name) },
+      { new: true }
+    );
+    expect(mockProductInstance.save).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.send).toHaveBeenCalledWith({
+      success: true,
+      message: "Product Updated Successfully",
+      products: mockProductInstance,
+    });
+  });
+
+  test("returns correct error message when product name is not provided", async () => {
+    req.fields.name = null;
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({ error: "Name is Required" });
+  });
+
+  test("returns correct error message when product description is not provided", async () => {
+    req.fields.description = null;
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({ error: "Description is Required" });
+  });
+
+  test("returns correct error message when product price is not provided", async () => {
+    req.fields.price = null;
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({ error: "Price is Required" });
+  });
+
+  test("returns correct error message when product category is not provided", async () => {
+    req.fields.category = null;
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({ error: "Category is Required" });
+  });
+
+  test("returns correct error message when product quantity is not provided", async () => {
+    req.fields.quantity = null;
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({ error: "Quantity is Required" });
+  });
+
+  test("returns correct error message when photo is more than 1mb in size", async () => {
+    req.files.photo.size = 1000001;
+
+    await updateProductController(req, res);
 
     expect(res.status).toHaveBeenCalledWith(500);
     expect(res.send).toHaveBeenCalledWith({

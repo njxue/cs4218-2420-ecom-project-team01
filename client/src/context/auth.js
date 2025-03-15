498094ce10f38e504ac4e9738a2043275f1726b4
njxue <ngjxue@gmail.com>
2025-03-15 11:44:00 +0800

    Fix token missing Bearer prefix

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -10,7 +10,7 @@ const AuthProvider = ({ children }) => {
   });
 
   //default axios
-  axios.defaults.headers.common["Authorization"] = auth?.token;
+  axios.defaults.headers.common["Authorization"] = `Bearer ${auth?.token}`;
 
   useEffect(() => {
     const data = localStorage.getItem("auth");

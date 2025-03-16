2da1928727c744759190fe8eef7e57fae83740fb
njxue <ngjxue@gmail.com>
2025-03-16 19:57:53 +0800

    Seed test db and config playwright to use test environ

--- config/db.js
+++ config/db.js
@@ -1,18 +1,44 @@
 import mongoose from "mongoose";
 import colors from "colors";
-import { createAdminUserIfNotExists } from "./setupUser";
+import dotenv from "dotenv";
+import { populateData } from "./populateData";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+dotenv.config();
+
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white
-    );
-    if (process.env.DEV_MODE === "development") {
-      await createAdminUserIfNotExists();
-    }
-  } catch (error) {
-    console.log(`Error in Mongodb ${error}`.bgRed.white);
+  switch (process.env.NODE_ENV) {
+    case "development":
+      console.log("Connecting to development database...");
+      await connectToDevDB();
+      break;
+    case "test":
+      console.log("Switching to in-memory test database...");
+      await connectToTestDB();
+      break;
+    default:
+      console.log("Invalid NODE_ENV value or missing environment variable.");
+      break;
   }
+};
+
+const connectToDevDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URL);
+  console.log(
+    `Connected To Mongodb Database [DEVELOPMENT] ${conn.connection.host}`
+      .bgMagenta.white
+  );
+};
+
+const connectToTestDB = async () => {
+  const mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+  console.log(
+    `Connected To in-memory Mongodb Database [TEST] ${uri}`.bgMagenta.white
+  );
+
+  await populateData();
 };
 
 export default connectDB;

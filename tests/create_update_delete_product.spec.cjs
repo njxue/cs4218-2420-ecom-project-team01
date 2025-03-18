74d4e0f113b1d6de32184d33f1e22804a560e531
njxue <ngjxue@gmail.com>
2025-03-18 10:22:35 +0800

    Add e2e test for create, update, delete product

---
+++ tests/create_update_delete_product.spec.cjs
@@ -0,0 +1,127 @@
+import { test, expect } from "@playwright/test";
+
+const getNameField = (page) =>
+  page.getByRole("textbox", { name: "write a name" });
+const getDescriptionField = (page) =>
+  page.getByRole("textbox", { name: "write a description" });
+const getPriceField = (page) => page.getByPlaceholder("write a Price");
+const getQuantityField = (page) => page.getByPlaceholder("write a quantity");
+const getUploadPhotoField = (page) => page.getByText("Upload Photo");
+const getCategoryField = (page) => page.locator("#rc_select_0");
+const getShippingField = (page) => page.locator("#rc_select_1");
+
+test("should create, update, then delete product successfully", async ({
+  page,
+}) => {
+  const uniqueId = Date.now();
+  const PRODUCT_DETAILS = {
+    name: `${uniqueId} Corgi t-shirt`,
+    description: `${uniqueId} A cute t-shirt`,
+    category: "Clothing",
+    price: "59.99",
+    quantity: "10",
+    shipping: "No",
+    photo: "corgi-tshirt.jpg",
+  };
+
+  const UPDATED_PRODUCT_DETAILS = {
+    name: `${uniqueId} To Kill A Mockingbird`,
+    description: `${uniqueId} To Kill A Mockingbird book`,
+    category: "Book",
+    price: "10.99",
+    quantity: "100",
+    shipping: "Yes",
+    photo: "to-kill-a-mockingbird.jpg",
+  };
+  // ================================ CREATE PRODUCT ================================
+  await page.goto("/dashboard/admin/create-product");
+
+  await getCategoryField(page).click();
+  await page.getByText(PRODUCT_DETAILS.category).click();
+
+  await getUploadPhotoField(page).click();
+  await getUploadPhotoField(page).setInputFiles(
+    `tests/assets/${PRODUCT_DETAILS.photo}`
+  );
+
+  await getNameField(page).click();
+  await getNameField(page).fill(PRODUCT_DETAILS.name);
+
+  await getDescriptionField(page).click();
+  await getDescriptionField(page).fill(PRODUCT_DETAILS.description);
+
+  await getPriceField(page).click();
+  await getPriceField(page).fill(PRODUCT_DETAILS.price);
+
+  await getQuantityField(page).click();
+  await getQuantityField(page).fill(PRODUCT_DETAILS.quantity);
+
+  await getShippingField(page).click();
+  await page.getByText(PRODUCT_DETAILS.shipping).click();
+
+  await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+
+  await expect(page.getByText("All Products List")).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: PRODUCT_DETAILS.name })
+  ).toBeVisible();
+  await expect(page.getByText(PRODUCT_DETAILS.description)).toBeVisible();
+
+  // ================================ UPDATE PRODUCT ================================
+  await page.getByRole("heading", { name: PRODUCT_DETAILS.name }).click();
+
+  await expect(getNameField(page)).toHaveValue(PRODUCT_DETAILS.name);
+  await expect(getDescriptionField(page)).toHaveValue(
+    PRODUCT_DETAILS.description
+  );
+  await expect(getPriceField(page)).toHaveValue(PRODUCT_DETAILS.price);
+  await expect(getQuantityField(page)).toHaveValue(PRODUCT_DETAILS.quantity);
+  await expect(page.getByText(PRODUCT_DETAILS.category)).toBeVisible();
+  await expect(page.getByText(PRODUCT_DETAILS.shipping)).toBeVisible();
+
+  await page.getByText(PRODUCT_DETAILS.category).click();
+  await page.getByText(UPDATED_PRODUCT_DETAILS.category).click();
+
+  await getUploadPhotoField(page).click();
+  await getUploadPhotoField(page).setInputFiles(
+    `tests/assets/${UPDATED_PRODUCT_DETAILS.photo}`
+  );
+
+  await getNameField(page).click();
+  await getNameField(page).fill(UPDATED_PRODUCT_DETAILS.name);
+
+  await getDescriptionField(page).click();
+  await getDescriptionField(page).fill(UPDATED_PRODUCT_DETAILS.description);
+
+  await getPriceField(page).click();
+  await getPriceField(page).fill(UPDATED_PRODUCT_DETAILS.price);
+
+  await getQuantityField(page).click();
+  await getQuantityField(page).fill(UPDATED_PRODUCT_DETAILS.quantity);
+
+  await page.getByText(PRODUCT_DETAILS.shipping).click();
+  await page.getByText(UPDATED_PRODUCT_DETAILS.shipping).click();
+
+  await page.getByRole("button", { name: "UPDATE PRODUCT" }).click();
+
+  await expect(page.getByText("All Products List")).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: UPDATED_PRODUCT_DETAILS.name })
+  ).toBeVisible();
+  await expect(
+    page.getByText(UPDATED_PRODUCT_DETAILS.description)
+  ).toBeVisible();
+
+  //================================ DELETE PRODUCT ================================
+  await page
+    .getByRole("heading", { name: UPDATED_PRODUCT_DETAILS.name })
+    .click();
+  await expect(getNameField(page)).toHaveValue(UPDATED_PRODUCT_DETAILS.name); // Wait for product details to load before clicking delete
+
+  page.once("dialog", async (dialog) => {
+    await dialog.accept("Yes");
+  });
+  await page.getByRole("button", { name: "DELETE PRODUCT" }).click();
+  await expect(page.getByText("All Products List")).toBeVisible();
+  await expect(page.getByText(UPDATED_PRODUCT_DETAILS.name)).not.toBeVisible();
+});

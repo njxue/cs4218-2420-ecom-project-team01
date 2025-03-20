b725b5e8fec51dbd6320ace230cd481366fc4c54
njxue <ngjxue@gmail.com>
2025-03-20 16:02:07 +0800

    Fix search unit test

--- client/src/context/search.test.js
+++ client/src/context/search.test.js
@@ -1,17 +1,23 @@
-import { act, renderHook } from "@testing-library/react";
-import { useSearch } from "./search";
+import { render, act, screen, renderHook } from "@testing-library/react";
+import axios from "axios";
+import React from "react";
+import { SearchProvider, useSearch } from "./search";
 import { describe } from "node:test";
 
 describe("Search Context", () => {
   test("default search keyword and results should be empty", () => {
-    const { result } = renderHook(() => useSearch());
+    const { result } = renderHook(() => useSearch(), {
+      wrapper: ({ children }) => <SearchProvider>{children}</SearchProvider>,
+    });
 
     expect(result.current[0]).toEqual({ keyword: "", results: [] });
   });
 
   test("should update keyword correctly", () => {
     const testKeyword = "test keyword";
-    const { result } = renderHook(() => useSearch());
+    const { result } = renderHook(() => useSearch(), {
+      wrapper: ({ children }) => <SearchProvider>{children}</SearchProvider>,
+    });
 
     act(() => {
       result.current[1]({ keyword: testKeyword, results: [] });
@@ -22,7 +28,9 @@ describe("Search Context", () => {
 
   test("should update results correctly", () => {
     const testResults = ["product 1", "product 2"];
-    const { result } = renderHook(() => useSearch());
+    const { result } = renderHook(() => useSearch(), {
+      wrapper: ({ children }) => <SearchProvider>{children}</SearchProvider>,
+    });
 
     act(() => {
       result.current[1]({ keyword: "", results: testResults });
